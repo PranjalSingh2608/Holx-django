2b013f2f3b5708efce897ee8fe6c8d45d1bdc6f0
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-08-23 15:25:33 +0530

    1.1

--- Holx/settings.py
+++ Holx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9x@3u99onr(m8o=_=buo8g@htu(p8a2-0@yma64@q*h%d7!emm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','holx.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','Holx.onrender.com']
 
 
 # Application definition
