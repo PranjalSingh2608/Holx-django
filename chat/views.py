3b27bb17aeae315c203750a3b4a7f741edbf4c4e
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-09-27 23:38:57 +0530

    9.2

--- chat/views.py
+++ chat/views.py
@@ -6,5 +6,14 @@ class ChatListCreateView(generics.ListCreateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
 
+    def get_queryset(self):
+        sender_id = self.request.query_params.get('sender_id')
+        receiver_id = self.request.query_params.get('receiver_id')
+        
+        if sender_id and receiver_id:
+            return Chat.objects.filter(sender_id=sender_id, receiver_id=receiver_id)
+        
+        return Chat.objects.none()
+
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
