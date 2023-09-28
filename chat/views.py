79884be00e5afd04ecf5af28cb9439b2d35a8079
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-09-28 16:14:24 +0530

    9.6

--- chat/views.py
+++ chat/views.py
@@ -1,24 +1,34 @@
-from rest_framework import generics
+from rest_framework import generics,permissions,status
 from .models import Chat
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializers import ChatSerializer
 
 class ChatListCreateView(generics.ListCreateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+        sender_id = self.request.user.id
+        receiver_id = self.request.data.get('receiver_id')
+        
+        if receiver_id:
+            serializer.save(sender_id=sender_id, receiver_id=receiver_id)
+        else:
+            return Response({"receiver_id": ["This field is required."]}, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(methods=['GET'])
-    def chat_history(self, request):
-        sender_id = request.query_params.get('sender_id')
-        receiver_id = request.query_params.get('receiver_id')
+    def get_queryset(self):
+        sender_id = self.request.query_params.get('sender_id')
+        receiver_id = self.request.query_params.get('receiver_id')
         
         if sender_id and receiver_id:
-            chats = Chat.objects.filter(sender_id=sender_id, receiver_id=receiver_id)
-            serializer = ChatSerializer(chats, many=True)
-            return Response(serializer.data)
+            return Chat.objects.filter(sender_id=sender_id, receiver_id=receiver_id)
         
-        return Response([])
+        return Chat.objects.none()
+
+
+
+
+
+
+
