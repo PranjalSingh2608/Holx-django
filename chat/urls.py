b31ecc2e4f265672d4830706b31c36d74501bdf7
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-09-28 14:53:49 +0530

    9.3

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,5 @@ from .views import ChatListCreateView
 
 urlpatterns = [
     path('', ChatListCreateView.as_view(), name='chat-list-create'),
+    path('<int:sender_id>/<int:receiver_id>/', ChatListCreateView.as_view(), name='chat-list-by-sender-receiver'),
 ]
