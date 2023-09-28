1064197b52b0aaa0e4f343047891d423b6bdc231
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-09-28 16:21:30 +0530

    9.7

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,4 @@ from .views import ChatListCreateView
 
 urlpatterns = [
     path('chat/', ChatListCreateView.as_view(), name='chat-list-create'),
-    path('chat_history/', ChatListCreateView.as_view({'get': 'chat_history'}), name='chat-history'),
 ]
