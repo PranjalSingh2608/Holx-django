c68d51a915b100c7c936eb609a8eaf092c00e138
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-09-29 22:17:36 +0530

    10.3

--- chat/models.py
+++ chat/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from application.models import Products
+
 class Chat(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE,related_name='sent_chats')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE,related_name='recieved_chats')
+    product = models.ForeignKey(Products, on_delete=models.CASCADE,default='23')
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
