7a7547cc9feca4c86f87b4ff92f1223bb76ce640
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-10-24 16:43:17 +0530

    11.5

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ChatListCreateView,ChatListView
 urlpatterns = [
     path('', ChatListCreateView.as_view(), name='chat-list-create'),
-    path('product/<int:product_id>/', ChatListView.as_view(), name='chat-list'),
+    path('product/<int:product_id>/sender/<int:sender_id>/receiver/<int:receiver_id>/', ChatListView.as_view(), name='chat-list'),
 ]
