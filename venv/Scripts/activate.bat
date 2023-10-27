332bb271b741110b55b141a9cf7ff34637fd2fa9
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2023-10-27 10:36:15 +0530

    12.7

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Django\buyit\Holx\venv
+set VIRTUAL_ENV=C:\Django\Holx-django\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
